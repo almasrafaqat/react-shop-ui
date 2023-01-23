2dcefc5760f2941733537687235167a934ca516a
Almas <almasrafaqat6@gmail.com>
2023-01-23 20:38:42 +0300

    Left Center Right Container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,36 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-    background-color: yellow;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    padding: 5px 10px;
+`
+
+const Left = styled.div`
+    flex: 1;
+
+`;
+
+const Center = styled.div`
+    flex: 1;
+`
+const Right = styled.div`
+    flex: 1;
 `;
 
 const Navbar = () => {
     return (
-        <Container>Navbar</Container>
+        <Container>
+            <Wrapper>
+            <Left>
+            LEft
+            </Left>
+            <Center>CEnter</Center>
+            <Right>Right</Right>
+            </Wrapper>
+        </Container>
     )
 }
 
