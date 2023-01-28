3084d05a1979b7b0fbd3d468c0ca168e7e4c6391
almasrafaqat <almasrafaqat6@gmail.com>
2023-01-28 21:37:43 +0300

    Black Laptop

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,7 +34,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transition: all 0.5s ease;
+  transition: all 1s ease;
   transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
@@ -92,7 +92,7 @@ const Slider = () => {
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => {
-          return <Slide bg={item.bg}>
+          return <Slide bg={item.bg} key={item.id}>
             <ImageContainer>
               <Image src={item.img} />
             </ImageContainer>
