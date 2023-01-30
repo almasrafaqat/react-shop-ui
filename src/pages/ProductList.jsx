52ed94d50e36e893e31288ec948d97ab45aa1e5d
almasrafaqat <almasrafaqat6@gmail.com>
2023-01-30 18:24:36 +0300

    ProductList Completed

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -7,18 +7,29 @@ import Products from "../components/Products";
 
 const Container = styled.div``;
 
-const Title = styled.h1``;
-
-const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
+const Title = styled.h1`
+  margin: 20px;
 `;
 
-const Filter = styled.div``;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
-const FilterText = styled.div``;
+const Filter = styled.div`
+  margin: 20px;
+`;
 
-const Select = styled.select``;
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
 
 const Option = styled.option``;
 
@@ -30,19 +41,35 @@ const ProductList = () => {
       <Title>Samsung</Title>
       <FilterContainer>
         <Filter>
-          <FilterText>Filter Products</FilterText>
+          <FilterText>Filter Products:</FilterText>
           <Select>
-            <Option>
-
+            <Option disabled selected>
+              Color
             </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
           </Select>
         </Filter>
         <Filter>
-          <FilterText>Sort Products</FilterText>
+          <FilterText>Sort Products:</FilterText>
           <Select>
-            <Option>
-                
-            </Option>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
