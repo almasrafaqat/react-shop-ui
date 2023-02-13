731e76643d0f30eca33269c27455dc79eda356e5
almasrafaqat <almasrafaqat6@gmail.com>
2023-02-13 21:28:29 +0300

    CartPage Completed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,14 +15,17 @@ const Title = styled.h1`
   text-align: center;
 `;
 const Top = styled.div`
+
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 20px;
+  
 `;
 
 const TopButton = styled.button`
   padding: 12px;
+  font-weight: 600;
   cursor: pointer;
   border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) =>
@@ -34,8 +37,8 @@ const TopTexts = styled.div``;
 
 const TopText = styled.span`
   text-decoration: underline;
-  margin: 0px 10px;
   cursor: pointer;
+  margin: 0px 10px;
 `;
 
 const Bottom = styled.div`
@@ -49,21 +52,20 @@ const Info = styled.div`
 
 const Product = styled.div`
   display: flex;
-  align-items: center;
   justify-content: space-between;
 `;
 const ProductDetails = styled.div`
+  flex: 2;
   display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex: 2s;
 `;
 const Image = styled.img`
-  width: 30%;
+  width: 200px;
 `;
 const Details = styled.div`
+  padding: 20px;
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
 `;
 const ProductName = styled.span``;
 const ProductId = styled.span``;
@@ -77,23 +79,26 @@ const ProductColor = styled.div`
 const ProductSize = styled.span``;
 const PriceDetails = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ProductAmountContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: 20px;
 `;
 
 const Amount = styled.span`
   font-size: 24px;
-  font-weight: 200;
   margin: 5px;
 `;
 
 const ProductPrice = styled.div`
-  font-size: 24px;
-  font-weight: 400;
-  margin: 5px 0px;
+  font-size: 30px;
+  font-weight: 200;
 `;
 
 const Hr = styled.hr`
@@ -104,6 +109,34 @@ const Hr = styled.hr`
 
 const Summary = styled.div`
   flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0px;
+  font-size: ${(props) => props.type === "total" && "24px"};
+  font-weight: ${(props) => props.type === "total" && "500"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  background-color: black;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: 600;
 `;
 
 const Cart = () => {
@@ -175,7 +208,26 @@ const Cart = () => {
               </PriceDetails>
             </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY </SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Newletter />
